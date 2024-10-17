2c8f5b63d243e90b00cd84a79ad5d001f5902ce4
tecwds <tecwds@163.com>
2024-10-17 16:36:11 +0800

    init: 初始化项目

--- fif-shadow/src/main/java/com/shadow/fifshadow/FifShadowApplication.java
+++ fif-shadow/src/main/java/com/shadow/fifshadow/FifShadowApplication.java
@@ -1,13 +1,16 @@
 package com.shadow.fifshadow;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class FifShadowApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FifShadowApplication.class, args);
+        log.info("FFiSystem 开始启动...");
     }
 
 }
--- fif-shadow/src/main/java/com/shadow/fifshadow/common/ResponseStatus.java
+++ fif-shadow/src/main/java/com/shadow/fifshadow/common/ResponseStatus.java
@@ -1,4 +1,18 @@
 package com.shadow.fifshadow.common;
 
+import lombok.Getter;
+
+@Getter
 public enum ResponseStatus {
+    SUCCESS(0, "ok"),
+    ERROR(1, "unknown error");
+
+    private final Integer code;
+    private final String  meesage;
+
+    ResponseStatus(int code ,String message) {
+        this.code = code;
+        this.meesage = message;
+    }
+
 }
