8aa1cd50fd94be0ab8d6120c430e0a88edf9f1fb
tecwds <tecwds@163.com>
2024-10-17 16:03:04 +0800

    init: ffi-shadow 初始化

---
+++ fif-shadow/src/main/java/com/shadow/fifshadow/FifShadowApplication.java
@@ -0,0 +1,13 @@
+package com.shadow.fifshadow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifShadowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifShadowApplication.class, args);
+    }
+
+}
